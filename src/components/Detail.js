5b63c85dbc68f513a12fa2b4505a4cb747841f60
Alin Alexandru Patrascu <patrascualinalexandru@gmail.com>
2022-03-17 16:55:51 +0100

    EDIT: fixes

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -65,6 +65,7 @@ const ImageTitle = styled.div`
   min-height: 170px;
   width: 35vw;
   min-width: 200px;
+  margin-top: 60px;
 
   img {
     width: 100%;
@@ -135,4 +136,5 @@ const Description = styled.div`
   line-height: 1.4;
   font-size: 20px;
   margin-top: 16px;
+  max-width: 760px;
 `
