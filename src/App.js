382e9402d885c700db4d411a5cc05b5b30eacb88
Alin Alexandru Patrascu <patrascualinalexandru@gmail.com>
2022-03-17 15:06:24 +0100

    ADD: login

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import Login from './components/Login';
+import { 
+  BrowserRouter as Router, 
+  Route,
+  Routes as Switch,
+  Link 
+} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login" element={<Login />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
